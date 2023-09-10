4a5c2ad9465d508b14936073086e1b7c5fe1fd98
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 13:41:15 -0400

    Auto-formatting code

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 Here are some improvements to the Python program:
 
 1. Import Only Necessary Libraries:
-   - Instead of importing the entire `pandas` library, import only the necessary `DataFrame` class.
-   - Instead of importing the entire `numpy` library, import only the necessary `arange` function.
-   - Instead of importing all the classes from `sklearn.linear_model`, import only the `LinearRegression` class.
-   - Instead of importing all the classes from `sklearn.model_selection`, import only the `train_test_split` function.
-   - Instead of importing all the classes from `sklearn.metrics`, import only the `mean_squared_error` function.
-   - Instead of importing the entire `matplotlib.pyplot` library, import only the necessary `pyplot` object.
+    - Instead of importing the entire `pandas` library, import only the necessary `DataFrame` class .
+    - Instead of importing the entire `numpy` library, import only the necessary `arange` function.
+    - Instead of importing all the classes from `sklearn.linear_model`, import only the `LinearRegression` class .
+    - Instead of importing all the classes from `sklearn.model_selection`, import only the `train_test_split` function.
+    - Instead of importing all the classes from `sklearn.metrics`, import only the `mean_squared_error` function.
+    - Instead of importing the entire `matplotlib.pyplot` library, import only the necessary `pyplot` object.
 
 2. Use Descriptive Variable Names:
-   - Rename variables such as `X`, `y`, `mse` in the `generate_recommendations` method to be more descriptive and meaningful.
+    - Rename variables such as `X`, `y`, `mse` in the `generate_recommendations` method to be more descriptive and meaningful.
 
 3. Improve User Input Handling:
-   - Add validation and error handling for the `risk_appetite` and `investment_goal` inputs in the `generate_investment_advice` method.
+    - Add validation and error handling for the `risk_appetite` and `investment_goal` inputs in the `generate_investment_advice` method.
 
 4. Add Error Handling for Empty Dataframes:
-   - Add error handling if the `analyze_spending` method is called when there are no expenses recorded.
+    - Add error handling if the `analyze_spending` method is called when there are no expenses recorded.
 
 5. Encapsulate Plotting Functionality:
-   - Create a separate method for plotting the bar chart in the `analyze_spending` method to improve code modularity and readability.
+    - Create a separate method for plotting the bar chart in the `analyze_spending` method to improve code modularity and readability.
 
 6. Improve Formatting of Revenue Streams:
-   - Improve the formatting of the revenue streams in the `generate_profitability` method by using bullet points.
+    - Improve the formatting of the revenue streams in the `generate_profitability` method by using bullet points.
 
 Here's the improved version of the program:
 
 ```python
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+
 
 class PersonalFinanceAdvisorySystem:
     def __init__(self):
@@ -41,16 +42,20 @@ class PersonalFinanceAdvisorySystem:
         self.goals = pd.DataFrame(columns=['Goal', 'Target'])
 
     def add_income(self, date, amount, category):
-        self.income = self.income.append({'Date': date, 'Amount': amount, 'Category': category}, ignore_index=True)
+        self.income = self.income.append(
+            {'Date': date, 'Amount': amount, 'Category': category}, ignore_index=True)
 
     def add_expense(self, date, amount, category):
-        self.expenses = self.expenses.append({'Date': date, 'Amount': amount, 'Category': category}, ignore_index=True)
+        self.expenses = self.expenses.append(
+            {'Date': date, 'Amount': amount, 'Category': category}, ignore_index=True)
 
     def set_budget(self, category, limit):
-        self.budget = self.budget.append({'Category': category, 'Limit': limit}, ignore_index=True)
+        self.budget = self.budget.append(
+            {'Category': category, 'Limit': limit}, ignore_index=True)
 
     def set_goal(self, goal, target):
-        self.goals = self.goals.append({'Goal': goal, 'Target': target}, ignore_index=True)
+        self.goals = self.goals.append(
+            {'Goal': goal, 'Target': target}, ignore_index=True)
 
     def analyze_spending(self):
         if self.expenses.empty:
@@ -71,10 +76,12 @@ class PersonalFinanceAdvisorySystem:
         plt.show()
 
     def generate_recommendations(self):
-        expenses_amounts = pd.to_numeric(self.expenses['Amount']).values.reshape(-1, 1)
+        expenses_amounts = pd.to_numeric(
+            self.expenses['Amount']).values.reshape(-1, 1)
         expense_indices = np.arange(len(expenses_amounts))
 
-        X_train, X_test, y_train, y_test = train_test_split(expenses_amounts, expense_indices, test_size=0.2, random_state=0)
+        X_train, X_test, y_train, y_test = train_test_split(
+            expenses_amounts, expense_indices, test_size=0.2, random_state=0)
 
         model = LinearRegression()
         model.fit(X_train, y_train)
@@ -90,7 +97,8 @@ class PersonalFinanceAdvisorySystem:
             return "Your spending is highly unstable. You need to take immediate action to reduce your expenses."
 
     def generate_investment_advice(self):
-        risk_appetite = input("Please enter your risk appetite (low / medium / high): ")
+        risk_appetite = input(
+            "Please enter your risk appetite (low / medium / high): ")
         investment_goal = input("Please enter your investment goal: ")
 
         if risk_appetite.lower() not in ('low', 'medium', 'high'):
@@ -111,11 +119,13 @@ class PersonalFinanceAdvisorySystem:
             {'stream': 'Subscription Model', 'description': 'Offer both a free version with limited features and a premium subscription plan with advanced features and access to specialized financial advice.'},
             {'stream': 'Partnerships', 'description': 'Collaborate with financial institutions or investment platforms to provide referral services, earning commission on investments made through recommendations.'},
             {'stream': 'Data Analysis Services', 'description': 'Offer aggregated and anonymized financial data analysis to financial institutions or market research companies, providing valuable insights and trends.'},
-            {'stream': 'Advertising and Partnership Opportunities', 'description': 'Partner with relevant financial service providers to display targeted ads or offers to users, generating advertising revenue.'},
+            {'stream': 'Advertising and Partnership Opportunities',
+                'description': 'Partner with relevant financial service providers to display targeted ads or offers to users, generating advertising revenue.'},
             {'stream': 'API Integration', 'description': 'Enable third-party developers to integrate the Intelligent Personal Finance Advisory System into their applications through API integration, generating licensing fees.'}
         ]
 
         return revenue_streams
+
 
 # Example usage of the PersonalFinanceAdvisorySystem class
 pfas = PersonalFinanceAdvisorySystem()
